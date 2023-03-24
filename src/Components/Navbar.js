486c49ca8b49e2aa7c787e8fb17e11d755ad1a1a
fuzzymfx <anubhabr50@gmail.com>
2023-03-25 02:21:11 +0530

    removing navbar render methods

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,79 +8,19 @@ import { useAuth } from "../contexts/AuthContext";
 import logo1 from '../assets/img/moon.png'
 import logo2 from '../assets/img/sun.png'
 import { Link } from "react-router-dom";
-import { getAnalytics, logEvent } from "firebase/analytics";
-const analytics = getAnalytics();
 
 export default function Header() {
-  const handleNotesClick = () => {
-    // Track link click
-    try{logEvent(analytics, 'select_content', { content_type: 'link', content_id: 'notes_link' });
-	}catch(error){
-		console.error('Error while logging select_content event:', error);
-	}
-  };
-  const handleTaskboardClick = () => {
-    // Track link click
-    try{logEvent(analytics, 'select_content', { content_type: 'link', content_id: 'taskboard_link' });
-  }catch(error){
-    console.error('Error while logging select_content event:', error);
-  }
-  };
-  const handleCommunityClick = () => {
-    // Track link click
-    try{logEvent(analytics, 'select_content', { content_type: 'link', content_id: 'community_link' });
-  }catch(error){
-    console.error('Error while logging select_content event:', error);
-  }
-  };
-  const handleContributionsClick = () => {
-    // Track link click
-    try{logEvent(analytics, 'select_content', { content_type: 'link', content_id: 'contributions_link' });
-  }catch(error){
-    console.error('Error while logging select_content event:', error);
-  }
-  };
-  const handleAboutClick = () => {
-    // Track link click
-    try{logEvent(analytics, 'select_content', { content_type: 'link', content_id: 'about_link' });
-  }catch(error){
-    console.error('Error while logging select_content event:', error);
-  }
-  };
-  const handleLoginClick = () => {
-    // Track link click
-    try{logEvent(analytics, 'select_content', { content_type: 'link', content_id: 'login_link' });
-  }catch(error){
-    console.error('Error while logging select_content event:', error);
-  }
-  };
-  const handleProfileClick = () => {
-    // Track link click
-    try{logEvent(analytics, 'select_content', { content_type: 'link', content_id: 'profile_link' });  
-  }catch(error){
-    console.error('Error while logging select_content event:', error);
-  }
-  };
-  const handleLogoClick = () => {
-    // Track link click
-    try{logEvent(analytics, 'select_content', { content_type: 'link', content_id: 'logo_link' });
-  }catch(error){
-    console.error('Error while logging select_content event:', error);
-  }
-  };
-
-
   const { currentUser } = useAuth();
   const [buttonText, setButtonText] = useState("Login");
 
   React.useEffect(() => {
-    if(currentUser && currentUser.displayName) {
-      if (currentUser.displayName.includes(" ")){
+    if (currentUser && currentUser.displayName) {
+      if (currentUser.displayName.includes(" ")) {
         var username = currentUser.displayName.slice(0, currentUser.displayName.indexOf(" "));
-         if(username.length> 15)
+        if (username.length > 15)
           username = username.slice(0, 15);
       }
-      else if(currentUser.displayName.length> 15) {
+      else if (currentUser.displayName.length > 15) {
         username = currentUser.displayName.slice(0, 15);
       }
       else {
@@ -89,9 +29,9 @@ export default function Header() {
       setButtonText(username);
     }
     else if (currentUser && currentUser.email) {
-      if (currentUser.email.includes("@")){
+      if (currentUser.email.includes("@")) {
         username = currentUser.email.slice(0, currentUser.email.indexOf("@"));
-        if(username.length> 15)
+        if (username.length > 15)
           username = username.slice(0, 15);
       }
       setButtonText(username);
@@ -117,13 +57,11 @@ export default function Header() {
       <Navbar className='navbar navbar-expand-lg navbar-dark py-4 cdin' expand="lg">
         <Container >
           <Nav >
-          <Navbar.Brand>
-          <Link 
-          onClick={handleLogoClick}
-          to="/">
-              <img src={logo1} alt="logo" width="30" height="30" className="d-inline-block align-top" />
-            </Link>
-          </Navbar.Brand>
+            <Navbar.Brand>
+              <Link to="/">
+                <img src={logo1} alt="logo" width="30" height="30" className="d-inline-block align-top" />
+              </Link>
+            </Navbar.Brand>
           </Nav>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
@@ -132,29 +70,19 @@ export default function Header() {
               style={{ maxHeight: '100px' }}
               navbarScroll
             >
-              <Link
-              onClick={handleNotesClick}
-              to="/notes" className="nav-link">Notes</Link>
-              {currentUser && <Link onclick={handleTaskboardClick}
-               to="/taskboard" className="nav-link">Tasks</Link>}
-              {!currentUser && <Link onclick={handleTaskboardClick} to="/login" className="nav-link">Tasks</Link>}
-              {currentUser && <Link onclick={handleCommunityClick}
-               to="/community" className="nav-link">Community</Link>}
-              {!currentUser && <Link onclick={handleCommunityClick}
-               to="/login" className="nav-link">Community</Link>}
-              {currentUser && <Link onclick={handleContributionsClick}
-               to="/contributions" className="nav-link">Contributions</Link>}
-              {!currentUser && <Link onclick={handleContributionsClick}
-               to="/login" className="nav-link">Contributions</Link>}
-              
+              <Link to="/notes" className="nav-link">Notes</Link>
+              {currentUser && <Link to="/taskboard" className="nav-link">Tasks</Link>}
+              {!currentUser && <Link to="/login" className="nav-link">Tasks</Link>}
+              {currentUser && <Link to="/community" className="nav-link">Community</Link>}
+              {!currentUser && <Link to="/login" className="nav-link">Community</Link>}
+              {currentUser && <Link to="/contributions" className="nav-link">Contributions</Link>}
+              {!currentUser && <Link to="/login" className="nav-link">Contributions</Link>}
+
             </Nav>
             <Nav>
-               <Link onClick={handleAboutClick} to="/aboutus" className="nav-link">About Us</Link>
-               {currentUser && <Link
-                onclick={handleProfileClick}
-               to="/profile" className="nav-link">{buttonText}</Link>}
-              {!currentUser && <Link onclick={handleProfileClick}
-               to="/login" className="nav-link">{buttonText}</Link>}
+              <Link to="/aboutus" className="nav-link">About Us</Link>
+              {currentUser && <Link to="/profile" className="nav-link">{buttonText}</Link>}
+              {!currentUser && <Link to="/login" className="nav-link">{buttonText}</Link>}
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -188,9 +116,9 @@ export default function Header() {
               {!currentUser && <Link to="/login" className="nav-link">Contributions</Link>}
             </Nav>
             <Nav>
-            <Link to="/aboutus" className="nav-link">About Us</Link>
-            {currentUser && <Link to="/profile" className="nav-link">{buttonText}</Link>}
-            {!currentUser && <Link to="/login" className="nav-link">{buttonText}</Link>}
+              <Link to="/aboutus" className="nav-link">About Us</Link>
+              {currentUser && <Link to="/profile" className="nav-link">{buttonText}</Link>}
+              {!currentUser && <Link to="/login" className="nav-link">{buttonText}</Link>}
             </Nav>
           </Navbar.Collapse>
         </Container>
